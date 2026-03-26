6abe1ac8730e3553e0c4de4cea419e1dbe8a7c58
Ayush Yadav <s24cseu1581@bennett.edu.in>
2026-03-26 19:55:01 +0530

    Cleaned Repo from junk

--- test-build.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process');
-try {
-  execSync('npm run build', { stdio: 'pipe' });
-  console.log("Success");
-} catch (e) {
-  console.log("ERROR OUTPUT:", e.stdout.toString() + e.stderr.toString());
-}
